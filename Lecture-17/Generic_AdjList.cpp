080a76794064dbce2b2e7393597fc6fe2bc30eee
kartik <kartik.mathur10@gmail.com>
2019-07-13 11:38:43 +0530

    Lecture-17

--- Lecture-17/Generic_AdjList.cpp
+++ Lecture-17/Generic_AdjList.cpp
@@ -9,6 +9,17 @@ using namespace std;
 template<typename T>
 class Graph{
 	map<T,list<T> > adjList;
+
+	void DFSHelper(T src,map<T,bool> &visited){
+		cout<<src<<", ";
+		visited[src] = true;
+
+		for(auto children:adjList[src]){
+			if(!visited[children]){
+				DFSHelper(children,visited);
+			}
+		}
+	}
 public:
 	void addEdge(T u,T v,bool bidir = true){
 		adjList[u].push_back(v);
@@ -82,39 +93,64 @@ public:
 
 		return dist[des];
 	}
+
+
+
+	void DFS(T src){
+		map<T,bool> visited;
+		int component = 1;
+
+		DFSHelper(src,visited);
+		// cout<<endl;
+
+		for(auto node:adjList){
+			if(!visited[node.first]){
+				component ++;
+				DFSHelper(node.first,visited);
+			}
+		}
+		cout<<endl;
+		cout<<"No of component : "<<component<<endl;
+	}
 };
 
 int main(){
 	Graph<int> g;
-	// g.addEdge(0,1);
-	// g.addEdge(0,4);
-	// g.addEdge(1,2);
-	// g.addEdge(4,2);
-	// g.addEdge(3,2);
-	// g.addEdge(3,4);
-	// g.addEdge(3,5);
+	g.addEdge(0,1);
+	g.addEdge(0,4);
+	g.addEdge(1,2);
+	g.addEdge(4,2);
+	g.addEdge(3,2);
+	g.addEdge(3,4);
+	g.addEdge(3,5);
 
+	g.addEdge(10,5);
+	g.addEdge(15,10);
+	g.addEdge(15,200);
+	g.addEdge(160,200);
+
+	g.DFS(0);
 	// cout<<g.SSSP(0,5)<<endl;
 
-	int board[50] = {0};
-	board[2] = 13;
-	board[5] = 2;
-	board[9] = 18;
-	board[18] = 11;
-	board[17]=-13;
-	board[20] = -14;
-	board[24] = -8;
-	board[25] = -10;
-	board[32] = -2;
-	board[34] = -22;
+	// int board[50] = {0};
+	// board[2] = 13;
+	// board[5] = 2;
+	// board[9] = 18;
+	// board[18] = 11;
+	// board[17]=-13;
+	// board[20] = -14;
+	// board[24] = -8;
+	// board[25] = -10;
+	// board[32] = -2;
+	// board[34] = -22;
 
-	for(int u=0;u<=36;u++){
-		for(int dice = 1; dice<=6;dice++){
-			int v = u+dice+board[u+dice];
-			g.addEdge(u,v,false);
-		}
-	}
-	cout<<g.SSSP(0,36)<<endl;
+	// for(int u=0;u<=36;u++){
+	// 	for(int dice = 1; dice<=6;dice++){
+	// 		int v = u+dice+board[u+dice];
+	// 		g.addEdge(u,v,false);
+	// 	}
+	// }
+	// cout<<g.SSSP(0,36)<<endl;
 	// g.addEdge("Putin","Trump",false);
 	// g.addEdge("Putin","Modi",false);
 	// g.addEdge("Trump","Modi",true);
---
+++ Lecture-17/WeightedGraph.cpp
@@ -0,0 +1,86 @@
+// WeightedGraph
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <list>
+#include <queue>
+#include <climits>
+#include <set>
+using namespace std;
+template<typename T>
+class Graph{
+	map<T,list<pair<T,int> > > h;
+public:
+	void addEdge(T u,T v,int dist,bool bidir = true){
+		h[u].push_back(make_pair(v,dist));
+		if(bidir){
+			h[v].push_back(make_pair(u,dist));
+		}
+	}
+	void Print(){
+		for(auto node:h){
+			cout<<node.first<<"-->";
+			for(auto neighbours:node.second){
+				cout<<"("<<neighbours.first<<","<<neighbours.second<<")";
+			}
+			cout<<endl;
+		}
+	}
+
+	int SSSP(T src,T des){
+		map<T,int> dist;
+		set<pair<int,T> > s;
+
+		for(auto node:h){
+			dist[node.first] = INT_MAX;
+		}
+
+		dist[src] = 0;
+		s.insert(make_pair(0,src));
+
+		while(!s.empty()){
+			auto p = (*s.begin());
+			int parent_dist = p.first;
+			T node = p.second;
+
+			s.erase(s.begin());
+
+			for(auto children:h[node]){
+				if(dist[children.first]>parent_dist+children.second){
+
+					auto f = s.find(make_pair(dist[children.first],children.first));
+					if(f!=s.end()){
+						s.erase(f);
+					}
+					dist[children.first] = parent_dist+children.second;
+					s.insert(make_pair(dist[children.first],children.first));
+
+				}
+			}
+		}
+
+		for(auto node:dist){
+			cout<<"Distance of "<<node.first<<" from "<<src<<" is "<<node.second<<endl;
+		}
+
+		return dist[des];
+	}
+
+};
+
+int main(){
+	Graph<string> g;
+
+	g.addEdge("Amritsar","Agra",1); 
+	g.addEdge("Amritsar","Jaipur",4);
+	g.addEdge("Delhi","Jaipur",2); 
+	g.addEdge("Delhi","Agra",1); 
+	g.addEdge("Bhopal","Agra",2); 
+	g.addEdge("Bhopal","Mumbai",3); 
+	g.addEdge("Jaipur","Mumbai",8);
+
+	// g.Print();
+	cout<<g.SSSP("Amritsar","Mumbai")<<endl;
+
+	return 0;
+}
